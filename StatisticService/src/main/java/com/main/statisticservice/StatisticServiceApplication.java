be5cc372b57b5432e7dad83a175114a92b9c10be
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-06-05 17:20:00 +0700

    Update statistic service

--- StatisticService/src/main/java/com/main/statisticservice/StatisticServiceApplication.java
+++ StatisticService/src/main/java/com/main/statisticservice/StatisticServiceApplication.java
@@ -3,7 +3,11 @@ package com.main.statisticservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
 
 @SpringBootApplication
 public class StatisticServiceApplication {
@@ -16,5 +20,10 @@ public class StatisticServiceApplication {
 	JsonMessageConverter converter() {
 		return new JsonMessageConverter();
 	}
+
+//	@Bean
+//	DefaultErrorHandler errorHandler(KafkaOperations<String, Object> template) {
+//		return new DefaultErrorHandler(new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
+//	}
 
 }
